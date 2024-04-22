1cc79735ad82e79b72c19d264ace4b0f9540b177
nisol <nisolmedinaue@gmail.com>
2024-04-22 17:37:49 +0200

    working on it

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 console.log();
+const questions = [
+    {
+        word: "Cadira",
+        options: [
+            { text: "Chair", correct: true },
+            { text: "Table", correct: false },
+            { text: "Window", correct: false }]
+    },
+    {
+        word: "sorra",
+        options: [
+            { text: "snow", correct: false },
+            { text: "sand", correct: true },
+            { text: "iron", correct: false }]
+    },
+    {
+        word: "fred",
+        options: [
+            { text: "Cold", correct: true },
+            { text: "wind", correct: false },
+            { text: "fresh", correct: false }]
+
+    },
+    {
+        word: "clau",
+       options: [
+        { text: "key", correct: true },
+        { text: "card", correct: false },
+        { text: "ring", correct: false }]
+    },
+]
+
+
+const wordElement = document.querySelector(".word");
+const  optionBtnElement= document.querySelector(".optionBtn");
+const NextButton= document.querySelector(".next");
+
+
